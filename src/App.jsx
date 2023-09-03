1c90945566d7e794ee18921af2b1ae31b412d5d0
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2023-09-03 13:53:43 -0300

    refatorando o código

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import Header from './Components/Header';
-import Body from './Components/Body';
-
+import Header from "./Components/Header";
+import Body from "./Components/Body";
 
 function App() {
-
-
   return (
     <>
       <Header />
       <Body />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
